f8f45680cfc47c2105d7bd16018055a9df3d6172
Bnguyen31 <bnguyen31@ggc.edu>
2024-09-13 20:32:41 -0400

    Add files via upload

--- src/PersonTester.java
+++ src/PersonTester.java
@@ -1,37 +1,20 @@
 public class PersonTester {
     public static void main(String[] args) {
-        // Create person1 using the constructor
+
+        // Create person1 using the no-argument constructor
         Person person = new Person();
 
-        // Get and print of the age of person1
+        // Get and print the age of person1
         int x = person.getAge();
         System.out.println(x);
 
-        // Creating another Person object using the constructor
-        Person person2 = new Person();
+        // Create more Person objects using the no-argument constructor
+        Person joshua = new Person();
+        Person christian = new Person();
 
-        // Set age for the second object and print
-        person2.setAge(500);
-        System.out.println(person2.getAge());
-
-        // Set name for the second object and print it with toString
-        person2.setName("Ikigai");
-        System.out.println(person2);
-
-        // Create more objects for other constructors to be used incase
-        Person Derox = new Person();
-        Person Maple = new Person();
-
-        // Create a new Person object with another constructor
-        Person m2 = new Person("Benil", 100);
-
-        // Get and print the age of m2
-        int y = m2.getAge();
-        System.out.println(y);
-
-        // Print the details of m2 using toString
-        System.out.println(m2);
+        // Set name for person1 to "Howard Roark" and print its details
+        person.setName("Howard Roark");
+        System.out.println(person);
     }
 }
-
 
